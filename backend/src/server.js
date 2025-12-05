80090f6451dc064fe2d0edd5ff581b76d5fec4c1
aniketocto <aniketocto@gmail.com>
2025-12-05 11:48:45 +0530

    name

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./dbconfig.js";
+import connectDB from "./Config/dbconfig.js";
 
 const app = express();
 
